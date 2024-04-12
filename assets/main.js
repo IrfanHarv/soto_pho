f6b17e677f5df31cf18f2cc07bfad393611138f3
IrfanHarv <irfanharv@gmail.com>
2024-04-12 11:07:26 +0700

    feat: add menu active

--- assets/main.js
+++ assets/main.js
@@ -1,10 +1,24 @@
-let menu = document.querySelector("#menu-icon");
-let navbar = document.querySelector(".navbar");
+document.addEventListener("DOMContentLoaded", () => {
+    let menu = document.querySelector("#menu-icon");
+    let navbar = document.querySelector(".navbar");
 
-menu.addEventListener("click", function () {
-    navbar.classList.toggle("active");
+    const menuItems = document.querySelectorAll('.navbar li a');
+    menuItems.forEach(item => {
+        item.addEventListener('click', (event) => {
+            // Remove active class from all menu items
+            menuItems.forEach(item => {
+                item.classList.remove('active');
+            });
+            // Add active class to the clicked menu item
+            event.target.classList.add('active');
+        });
+    });
+
+    menu.addEventListener("click", function () {
+        navbar.classList.toggle("active");
+    });
+
+    window.onscroll = () => {
+        navbar.classList.remove("active");
+    };
 });
-
-window.onscroll = () => {
-    navbar.classList.remove("active");
-};
